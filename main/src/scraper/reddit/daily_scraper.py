a790bd62859bfece8128978d56c2670f427db3b1
joshuawong96 <51185596+joshuawong96@users.noreply.github.com>
2022-01-15 16:27:56 +0800

    update reddit daily_scraper + create sample data
    now saves into one single file

--- main/src/scraper/reddit/daily_scraper.py
+++ main/src/scraper/reddit/daily_scraper.py
@@ -3,10 +3,9 @@ from dotenv import load_dotenv
 from ....utils import file_utils
 import datetime
 import os
-import json
+
 import praw
 import math
-import pprint
 
 # Load environment variables
 load_dotenv()
@@ -23,44 +22,38 @@ try:
 except:
     print('Failed to connect to Reddit API')
 
-
 # Initialise scraping cut-off date
 cutoff_days = int(os.getenv('CUTOFF_DAYS'))
 start_datetime = datetime.datetime.now()
 stop_datetime = start_datetime - datetime.timedelta(days = cutoff_days)
 
-# Create new json file with current date as filename
-new_filename = str(start_datetime.date())
-new_filepath = f'./main/src/scraper/reddit/data/{new_filename}.json'
-file_utils.create_json(new_filepath)
-
-# Initialise post counter for tracking
+# Create storage dictionaries & initialise post counter for tracking
+submissions_dict = {}
 counter = 0
 
-# Read newly created json file
-with open(new_filepath, 'a') as file:
+# Iterate through list of newest submissions in selected Subreddit 
+for sub in reddit.subreddit('Singapore').new(limit=math.inf):
+    counter += 1
 
-    # Iterate through list of newest submissions in selected Subreddit 
-    for sub in reddit.subreddit('Singapore').new(limit=math.inf):
-        counter += 1
+    # Stop loading new posts older than 2 weeks
+    submission_created_datetime = datetime.datetime.fromtimestamp(sub.created_utc) #changes unixtimestamp to a readable format
+    if stop_datetime > submission_created_datetime:
+        # Stops scraping if  submission_created_datetime < stop_datetime
+        # Note that earlier dates are considered smaller than later dates, i.e. 2022-01-14 < 2022-01-15
+        break
 
-        # Stop loading new posts older than 2 weeks
-        submission_created_datetime = datetime.datetime.fromtimestamp(sub.created_utc) # changes unixtimestamp to a readable format
-        if  submission_created_datetime < stop_datetime: 
-            # Stops scraping if  submission_created_datetime < stop_datetime
-            # Note that earlier dates are considered smaller than later dates, i.e. 2022-01-14 < 2022-01-15
-            break
+    submission = vars(sub) # Returns the __dict__ attribute of a given submission
+    submission['comments'] = {} # Initialise comments dictionary
 
-        submission = vars(sub) # Returns the __dict__ attribute of a given submission
-        submission['comments'] = {} # Initialise comments dictionary
-        
-        # Load comments for each submission
-        sub.comments.replace_more(limit = None)
-        for comment in sub.comments.list(): # Loop through the list of comments for the submission
-            comment = vars(comment) # Returns the __dict__ attribute of a given submission
-            submission['comments'][comment['id']] = comment # Saves the comment to comments dictionary
+    # Load comments for each submission
+    sub.comments.replace_more(limit = None)
+    for comment in sub.comments.list(): # Loop through the list of comments for the submission
+        comment = vars(comment) # Returns the __dict__ attribute of a given submission
+        submission['comments'][comment['id']] = comment # Saves the comment to comments dictionary
 
-        print(f'Post {counter} saved [{len(sub.comments.list())} comments].')
+    submissions_dict[submission['id']] = submission
+    print(f'Post {counter} saved [{len(sub.comments.list())} comments].')
 
-        # Append individual submission to json file
-        json.dump(submission, file, ensure_ascii=False, indent=4, default=str)
+# Save file
+file_utils.save_json(f'./main/src/scraper/reddit/data/{start_datetime.date()}.json', submissions_dict)
+print('Daily scraping completed.')
