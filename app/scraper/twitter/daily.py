f4e5ce511c533128bb1c38a32ef259883096e62f
joshuawong96 <51185596+joshuawong96@users.noreply.github.com>
2022-01-21 13:56:16 +0800

    update twitter daily and historical scraper

--- app/scraper/twitter/daily.py
+++ app/scraper/twitter/daily.py
@@ -16,16 +16,17 @@ stop_datetime = start_datetime - datetime.timedelta(days=cutoff_days)
 
 # Configure up Twint
 c = twint.Config()
-# c.Geo = "1.367416,103.809003,25km"
 c.Near = "Singapore"  # Set geographic location to near Singapore
 c.Lang = "en"  # Set language to english
-c.Output = f"./main/src/scraper/twitter/data/{start_datetime.date()}.json"  # Save output in current directory containing python script (edit later)
+c.Limit = sys.maxsize  # Set tweet limit to unlimited
+c.Retweets = True  # Include retweets done by user
+
 c.Since = str(stop_datetime.strftime("%Y-%m-%d %H:%M:%S"))  # Set end date for collection
 c.Until = str(start_datetime.strftime("%Y-%m-%d %H:%M:%S"))  # Set start date for collection
-c.Store_json = True  # Store to json
-c.Limit = sys.maxsize  # Set tweet limit to unlimited
-c.Count = True
-c.Retweets = True  # Include retweets done by user
+
+# Saves json to same folder as scraper (edit later)
+c.Store_json = True  
+c.Output = f"./app/scraper/twitter/{start_datetime.date()}.json"
 
 # Run Twint
 twint.run.Search(c)
--- app/scraper/twitter/historical.py
+++ app/scraper/twitter/historical.py
@@ -13,15 +13,17 @@ start_datetime = datetime.datetime.now()
 
 # Set up Twint
 c = twint.Config()
-c.Store_json = True  # Store to json
-c.Since = "2018-01-01 00:00:00"  # Set end date for collection
-c.Until = str(start_datetime.strftime("%Y-%m-%d %H:%M:%S"))  # Set start date for collection
-c.Retweets = True  # Include retweets done by user
 c.Near = "Singapore"  # Set geographic location to near Singapore
 c.Lang = "en"  # Set language to english
 c.Limit = sys.maxsize  # Set tweet limit to unlimited
-# c.Search = 'search term' # Set search term (optional)
-c.Output = "./main/src/scraper/twitter/data/historical.json"  # Save output in current directory containing python script (edit later)
+c.Retweets = True  # Include retweets done by user
+
+c.Since = "2018-01-01 00:00:00"  # Set end date for collection
+c.Until = str(start_datetime.strftime("%Y-%m-%d %H:%M:%S"))  # Set start date for collection
+
+# Saves json to same folder as scraper (edit later)
+c.Store_json = True 
+c.Output = "./app/scraper/twitter/historical.json"
 
 # Run Twint
 twint.run.Search(c)
