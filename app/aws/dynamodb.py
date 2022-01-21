c35cdc7c2753f9b922aec74ceb816923114cc6d6
joshuawong96 <51185596+joshuawong96@users.noreply.github.com>
2022-01-21 17:23:18 +0800

    added sample dynamodb table creation script

--- app/aws/dynamodb.py
+++ app/aws/dynamodb.py
@@ -1,6 +1,53 @@
 import boto3
 
-# Get Dynamodb service resource
-db = boto3.resource("dynamodb")
+# # Get Dynamodb service resource / client
+# client = boto3.client("dynamodb")
+# db = boto3.resource("dynamodb") # Higher level API
 
-# Create tables
+# # Create tables
+# __TableName__ = "People"
+# Primary_Column_Name = 'Sr'
+# Primary_Key = 1
+# columns = ["Age", "First", "Last"]
+
+# table = db.Table(__TableName__)
+
+
+def create_movie_table(dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.create_table(
+        TableName='Movies',
+        KeySchema=[
+            {
+                'AttributeName': 'year',
+                'KeyType': 'HASH'  # Partition key
+            },
+            {
+                'AttributeName': 'title',
+                'KeyType': 'RANGE'  # Sort key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'year',
+                'AttributeType': 'N'
+            },
+            {
+                'AttributeName': 'title',
+                'AttributeType': 'S'
+            },
+
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    )
+    return table
+
+
+if __name__ == '__main__':
+    movie_table = create_movie_table()
+    print("Table status:", movie_table.table_status)
