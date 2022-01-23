205e8236f249958f910968cd109431e83f6e1f4d
Cjianyu98 <Cjianyu98@gmail.com>
2022-01-23 22:32:37 +0800

    Refactor code for aws s3 setup

--- app/aws/s3.py
+++ app/aws/s3.py
@@ -1,10 +1,10 @@
 import json
+import os
 
 import boto3
 from dotenv import load_dotenv
 
 from ..exception import AwsBucketCreationError
-from ..utils import aws_utils
 from .constant import SOCIAL_MEDIA_PLATFORMS
 
 # Load environment variables
@@ -12,26 +12,54 @@ load_dotenv()
 
 
 def s3_setup():
-    # Establish s3 client
-    client = boto3.client("s3")
+    """
+    This function creates AWS S3 buckets for each of the social media platform to store raw data everyday.
+    Bucket policy will be implemented for each S3 bucket.
+    """
 
-    # Create buckets for project
+    client = boto3.client("s3")  # Establish s3 client
+
     try:
         for platform in SOCIAL_MEDIA_PLATFORMS:
             bucket_name = f"smt483tls-{platform}-bucket"
-            bucket_response = aws_utils.create_s3_bucket(client=client, bucket_name=bucket_name)
+            bucket_response = client.create_bucket(
+                Bucket=bucket_name,
+                CreateBucketConfiguration={"LocationConstraint": os.getenv("AWS_REGION")},
+            )
 
             # if bucket_response['HTTPStatusCode'] > 299:
             #     raise AwsBucketCreationError(BUCKET_NAME)
 
-            # Create s3 bucket policy
-            policy = aws_utils.create_s3_policy_object(bucket_name=bucket_name)
+            policy = create_s3_policy_object(bucket_name=bucket_name)
             bucket_policy = json.dumps(policy)
-            policy_response = aws_utils.create_bucket_policy(
-                client=client, bucket_name=bucket_name, policy=bucket_policy
-            )
+            policy_response = client.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy)
 
             # if policy_response['HTTPStatusCode'] > 299:
             #     raise AwsBucketCreationError(BUCKET_NAME)
     except Exception as e:
         print(e)
+
+
+def create_s3_policy_object(bucket_name: str) -> dict:
+    """
+    Create policy object for AWS S3 based on the template.
+
+    Args:
+        bucket_name (str): Bucket name for the S3 bucket
+
+    Returns:
+        dict: policy object
+    """
+    return {
+        "Version": "2012-10-17",
+        "Id": "Smt483-proj-bucket-policy",
+        "Statement": [
+            {
+                "Sid": "Smt483-proj-bucket-policy1",
+                "Action": "s3:*",
+                "Effect": "Allow",
+                "Resource": f"arn:aws:s3:::{bucket_name}/*",
+                "Principal": {"AWS": [f"arn:aws:iam::{os.getenv('AWS_ACCOUNT_ID')}:root"]},
+            }
+        ],
+    }
