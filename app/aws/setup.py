06cdc031741fbf8f13573c688dabf3c082e1bba3
Cjianyu98 <Cjianyu98@gmail.com>
2022-01-30 19:06:58 +0800

    Update setup.py

--- app/aws/setup.py
+++ app/aws/setup.py
@@ -1,4 +1,6 @@
-from .lambda_service import iam_role
+from .lambda_service import create_iam_role_permission_policy, create_lambda_iam_role
 from .s3 import s3_setup
 
-# s3_setup()
+s3_setup()
+permission_policy_name = create_iam_role_permission_policy()
+create_lambda_iam_role(permission_policy_name)
