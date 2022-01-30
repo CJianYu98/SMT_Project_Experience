568c03085f122f57b361e8799a437c0252fb63ef
joshuawong96 <51185596+joshuawong96@users.noreply.github.com>
2022-01-30 19:29:30 +0800

    Update constant.py

--- app/aws/constant.py
+++ app/aws/constant.py
@@ -1,6 +1,3 @@
-from re import S
-
-
 SOCIAL_MEDIA_PLATFORMS = ["reddit", "instagram", "facebook", "youtube", "twitter"]
 LAMBDA_IAM_ROLE_NAME = "LambdaRole"
 EVENTS_SCHEDULER_RULE = "DailySchedulerRule"
