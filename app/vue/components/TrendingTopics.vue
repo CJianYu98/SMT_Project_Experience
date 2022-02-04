6378919d9c2bba2142426a46d763c2df19220d66
jhin-yee <jhinyee.ong.2019@scis.smu.edu.sg>
2022-02-04 16:34:00 +0800

    create trending topics component

---
+++ app/vue/components/TrendingTopics.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card
+    elevation="3"
+    class="mx-8 mt-8"
+  >
+    <v-card-title>Trending Topics</v-card-title>
+  
+  </v-card>
+</template>
+
+<script>
+export default {
+  top5topics: [
+    {
+    topic: 'GE2020', 
+    mentions: 294940, 
+    sentiment: {
+      negative: 0.2, neutral: 0.6, positive: 0.2
+      }
+    },
+    {
+    topic: 'GE2024', 
+    mentions: 29494, 
+    sentiment: {
+      negative: 0.2, neutral: 0.6, positive: 0.2
+      }
+    }
+  ]
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ app/vue/pages/dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row>
+    <v-col cols="5">
+      <TrendingTopics />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import TrendingTopics from '@/components/TrendingTopics.vue'
+export default {
+  components: { 
+    TrendingTopics 
+  },
+  
+}
+</script>
+
+
+<style>
+
+</style>
