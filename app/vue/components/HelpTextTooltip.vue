096b4aa9f1a2fd88b8dab791ed0438a4b9d2b144
jhin-yee <jhinyee.ong.2019@scis.smu.edu.sg>
2022-02-11 15:45:36 +0800

    Create HelpTextTooltip component

---
+++ app/vue/components/HelpTextTooltip.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-left ml-2 mb-1">
+    <v-tooltip top max-width="180px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-icon
+          color="deep-purple"
+          v-bind="attrs"
+          v-on="on"
+        >
+          mdi-help-circle-outline
+        </v-icon>
+      </template>
+      <span> {{ helpText }} </span>
+    </v-tooltip>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { 
+    helpText: {
+      type: String,
+      required: true,
+    }
+  },
+}
+</script>
+
+<style>
+  
+</style>
+
