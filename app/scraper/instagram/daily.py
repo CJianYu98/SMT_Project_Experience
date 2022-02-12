848ec883c436bf55ff717400e12ff2313d2b5ad0
Cjianyu98 <Cjianyu98@gmail.com>
2022-02-12 19:43:38 +0000

    Update instagram daily scraper

--- app/scraper/instagram/daily.py
+++ app/scraper/instagram/daily.py
@@ -1,54 +1,78 @@
 # Import packages
-import datetime
+from datetime import datetime, timedelta
 import json
 import math
 import os
+import telegram_send
+import time
 
 import instaloader
+import boto3
+import botocore.exceptions
+import pytz
 from dotenv import load_dotenv
+from loguru import logger
 
 # Load environment variables
 load_dotenv()
 
-# Initialise scraping cut-off date
-cutoff_days = int(os.getenv("CUTOFF_DAYS"))
-start_datetime = datetime.datetime.now()
-stop_datetime = start_datetime - datetime.timedelta(days=cutoff_days)
+# Change to file directory
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-# Create an instance of Instaloader class
-loader = instaloader.Instaloader(compress_json=False, download_comments=True, quiet=True)
-
-# Enter your Instagram handle and password
+# Constants and variables
 ACCOUNT = os.getenv("INSTAGRAM_ACCOUNT")
 PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
 
-# Upon successful authentication, you should see a message saying Authentication OK.
-# Otherwise, check your login details
+cutoff_days = int(os.getenv("CUTOFF_DAYS"))
+start_datetime = datetime.now()
+stop_datetime = start_datetime - timedelta(days=cutoff_days)
+date = start_datetime.date()
+sg_datetime = datetime.now(TIMEZONE)
+
+tele_start_msg = f"INSTAGRAM DAILY --> Daily data crawling started at {sg_datetime}"
+tele_end_msg = "INSTAGRAM DAILY --> \n"
+
+
 try:
+    telegram_send.send(messages=[tele_start_msg])
+    logger.info(f"Daily data crawling started at {sg_datetime}")
+
+    # Create an instance of Instaloader class and login
+    loader = instaloader.Instaloader(compress_json=False, download_comments=True, quiet=True)
     loader.login(ACCOUNT, PASSWORD)
-    print("Authentication OK")
-except:
-    print("Error during authentication")
 
-# Get profiles to scrape
-profiles = json.load(open('./app/scraper/accounts_to_scrape/instagram.json'))
+    # Get profiles to scrape
+    profiles = json.load(open('../accounts_to_scrape/instagram.json'))
 
-# Iterate through profiles
-for p in profiles.values():
-    # Get profile object
-    profile = instaloader.Profile.from_username(loader.context, p)
+    # Change to data directory where data will be downloaded
+    os.chdir('./data')
 
-    # Get all posts from the profile in a generator
-    posts = profile.get_posts()
+    # Iterate through profiles
+    for p in profiles.values():
+        # Get profile object
+        try: 
+            profile = instaloader.Profile.from_username(loader.context, p)
 
-    for post in posts:
-        # Download post if it is within 2 weeks
-        if post.date_local <= stop_datetime:
-            break
-        try:
-            os.chdir('./app/scraper/instagram/data')
-        except:
-            pass
-        loader.download_post(post, target=f"@{p}")
-    print(f'Posts for @{p} scraped.')
-print('Daily scraper completed.')
+            # Get all posts from the profile in a generator
+            posts = profile.get_posts()
+
+            for post in posts:
+                # Download post if it is within 2 weeks
+                if post.date_local <= stop_datetime:
+                    break
+                
+                loader.download_post(post, target=f"@{p}")
+                time.sleep(3)
+            logger.debug(f'Posts for @{p} scraped.')
+        except Exception as e:
+            telegram_send.send(messages=[f"Error: {e}\nContinuing on..."])
+            logger.debug("Error: {e}\nContinuing on...")
+            continue
+    
+    tele_end_msg += f'Daily scraper completed for {date}.'
+
+except Exception as e:
+    tele_end_msg += f"Error occured.\n{e}\n"
+    logger.exception("Error occured.")
+
+telegram_send.send(messages=[tele_end_msg])
