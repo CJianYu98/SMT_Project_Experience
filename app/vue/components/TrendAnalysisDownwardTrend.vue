daf2cefb6440feb55721a503156c3b28efb056c8
jhin-yee <jhinyee.ong.2019@scis.smu.edu.sg>
2022-02-14 16:12:29 +0800

    create components for trend text

---
+++ app/vue/components/TrendAnalysisDownwardTrend.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-icon
+      color="red lighten-1"
+    >
+      mdi-trending-down
+    </v-icon>
+    <span class="red--text"> {{ percentageDecrease * 100 }}% </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { 
+    percentageDecrease: {
+      type: Number,
+      required: true,
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ app/vue/components/TrendAnalysisUpwardTrend.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-icon
+      color="light-green lighten-1"
+    >
+      mdi-trending-up
+    </v-icon>
+    <span class="light-green--text"> {{ percentageIncrease * 100 }}% </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { 
+    percentageIncrease: {
+      type: Number,
+      required: true,
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
+
