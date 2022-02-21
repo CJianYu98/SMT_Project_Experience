540c07514d0f0490b8b39cc17850f19a2b3f8694
ctianle7 <cheowtl@gmail.com>
2022-02-22 01:51:06 +0800

    create drop down select component

---
+++ app/vue/components/DropDownSelect.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="d-block">
+        <v-select
+            d-block
+            v-model="viewSelected"
+            :items="viewFilter"
+            item-text="view"
+            label="View"
+            outlined
+            dense
+        >
+        </v-select>
+    </div>
+</template>
+
+<script>
+  export default {
+    props:{
+      viewFilter:{
+        required: true
+      }
+    },
+    data: () => ({
+      items: [],
+    //   viewSelected: "Number of Likes",
+    //   viewFilter: [{view: 'Number of Likes'}, {view: 'Number of Comments'}, {view: 'Number of Shares'}]
+    }),
+  }
+</script>
