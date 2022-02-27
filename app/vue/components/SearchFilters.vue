0f5854ac9de4bc395e5795300f021d07908315d6
jhin-yee <jhinyee.ong.2019@scis.smu.edu.sg>
2022-02-27 19:12:30 +0800

    remove showing text in v-autocomplete in chip form

--- app/vue/components/SearchFilters.vue
+++ app/vue/components/SearchFilters.vue
@@ -12,56 +12,16 @@
             v-model="autocompleteModel"
             :items="items"
             :loading="isLoading"
-            chips
             clearable
             hide-details
             hide-selected
             item-text="name"
             item-value="symbol"
-            label="Enter a keyword you are interested in!"
+            label="Enter a query!"
             solo
             dense
             return-object
           >
-            <!-- :label="selectedTrendingQuery" -->
-                      <!-- return-object -->
-            <!-- <template v-slot:no-data>
-              <v-list-item>
-                <v-list-item-title>
-                  Search for your favorite
-                  <strong>Cryptocurrency</strong>
-                </v-list-item-title>
-              </v-list-item>
-            </template> -->
-            <template #selection="{ attr, on, item, selected }">
-              <v-chip
-                v-bind="attr"
-                :input-value="selected"
-                class="primary--text"
-                v-on="on"
-                small
-              >
-                <!-- <v-icon left>
-                  mdi-bitcoin
-                </v-icon> -->
-                <span v-text="item.name"></span>
-              </v-chip>
-            </template>
-            <template #item="{ item }">
-              <v-list-item-avatar
-                color="indigo"
-                class="text-h5 font-weight-light white--text"
-              >
-                {{ item.name.charAt(0) }}
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title v-text="item.name"></v-list-item-title>
-                <v-list-item-subtitle v-text="item.symbol"></v-list-item-subtitle>
-              </v-list-item-content>
-              <!-- <v-list-item-action>
-                <v-icon>mdi-bitcoin</v-icon>
-              </v-list-item-action> -->
-            </template>
           </v-autocomplete>
 
           <v-autocomplete
@@ -70,52 +30,16 @@
             :items="items"
             :loading="isLoading"
             :search-input.sync="search"
-            chips
             clearable
             hide-details
             hide-selected
             item-text="name"
             item-value="symbol"
-            label="Enter a keyword you are interested in!"
+            label="Enter a query!"
             solo
             dense
             return-object
           >
-                      <!-- return-object -->
-            <!-- <template v-slot:no-data>
-              <v-list-item>
-                <v-list-item-title>
-                  Search for your favorite
-                  <strong>Cryptocurrency</strong>
-                </v-list-item-title>
-              </v-list-item>
-            </template> -->
-            <template #selection="{ attr, on, item, selected }">
-              <v-chip
-                v-bind="attr"
-                :input-value="selected"
-                class="primary--text"
-                v-on="on"
-                small
-              >
-                <span v-text="item.name"></span>
-              </v-chip>
-            </template>
-            <template #item="{ item }">
-              <v-list-item-avatar
-                color="indigo"
-                class="text-h5 font-weight-light white--text"
-              >
-                {{ item.name.charAt(0) }}
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title v-text="item.name"></v-list-item-title>
-                <v-list-item-subtitle v-text="item.symbol"></v-list-item-subtitle>
-              </v-list-item-content>
-              <!-- <v-list-item-action>
-                <v-icon>mdi-bitcoin</v-icon>
-              </v-list-item-action> -->
-            </template>
           </v-autocomplete>
         </v-col>
         <v-spacer></v-spacer>
