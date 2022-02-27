94998c8afef8ad463a5cef3eb6eb1dd6cde587ea
Cjianyu98 <Cjianyu98@gmail.com>
2022-02-27 15:43:54 +0800

    Update Facebook routers and schemas

--- app/routers/facebook.py
+++ app/routers/facebook.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Query, Path, HTTPException
 from ..database.connect import db
-from ..schema.facebook import FbPostRes, FbDailyStatsRes, FbTrendRes
+from ..schema.facebook import FbPostRes, FbStatsRes, FbTrendRes
 from typing import List
 from datetime import datetime, timedelta
 
@@ -44,7 +44,7 @@ async def get_posts(
     return posts
 
 
-@router.get("/get-daily-stats", response_model=List[FbDailyStatsRes])
+@router.get("/get-daily-stats", response_model=List[FbStatsRes])
 async def get_daily_stats(
     start_date: str = Query(..., regex="\d\d\d\d-\d\d-\d\d"),
     end_date: str = Query(..., regex="\d\d\d\d-\d\d-\d\d"),
@@ -60,7 +60,7 @@ async def get_daily_stats(
         HTTPException: Raised when no Facebook data found in the given date range
 
     Returns:
-        List[FbDailyStatsRes]: List of Pydantic response model
+        List[FbStatsRes]: List of Pydantic response model
     """
     start_date = datetime.strptime(start_date, "%Y-%m-%d")
     end_date = datetime.strptime(end_date, "%Y-%m-%d") + timedelta(days=1)
@@ -81,6 +81,58 @@ async def get_daily_stats(
         },
         {"$addFields": {"date": "$_id"}},
         {"$project": {"_id": False}},
+        {"$sort": {"date": 1}}
+    ]
+    stats = list(db.fb_posts.aggregate(db_query))
+
+    if len(stats) == 0:
+        raise HTTPException(
+            status_code=404,
+            detail=f"No data found within date period {start_date} to {end_date}",
+        )
+
+    return stats
+
+
+@router.get("/get-hourly-stats", response_model=List[FbStatsRes])
+async def get_hourly_stats(
+    start_date: str = Query(..., regex="\d\d\d\d-\d\d-\d\d"),
+    end_date: str = Query(..., regex="\d\d\d\d-\d\d-\d\d"),
+):
+    """
+    Get Facebook hourly statistics within a date range. To add one more day to the end_date argument in order to include data within that date.
+
+    Args:
+        start_date (str): Start date of the date range (Inclusive). Defaults to Query(..., regex="\d\d\d\d-\d\d-\d\d").
+        end_date (str): End date of the date range (Exclusive). Defaults to Query(..., regex="\d\d\d\d-\d\d-\d\d").
+
+    Raises:
+        HTTPException: Raised when no Facebook data found in the given date range
+
+    Returns:
+        List[FbStatsRes]: List of Pydantic response model
+    """
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date, "%Y-%m-%d") + timedelta(days=1)
+
+    db_query = [
+        {"$match": {"created_time": {"$gte": start_date, "$lte": end_date}}},
+        {
+            "$group": {
+                "_id": {
+                    "year": {"$year": "$created_time"},
+                    "month": {"$month": "$created_time"},
+                    "day": {"$dayOfMonth": "$created_time"},
+                    "hour": {"$hour": "$created_time"}
+                },
+                "total_comments": {"$sum": "$comments_cnt"},
+                "total_likes": {"$sum": "$likes_cnt"},
+                "count": {"$sum": 1},
+            }
+        },
+        {"$addFields": {"date": "$_id"}},
+        {"$project": {"_id": False}},
+        {"$sort": {"date": 1}}
     ]
     stats = list(db.fb_posts.aggregate(db_query))
 
