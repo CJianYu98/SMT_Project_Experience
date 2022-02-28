53f6a2037eec2c12f880e2ad711d7d29501f7a3c
ctianle7 <cheowtl@gmail.com>
2022-02-28 12:20:57 +0800

    update complains related comments

--- app/vue/components/ComplaintsRelatedComment.vue
+++ app/vue/components/ComplaintsRelatedComment.vue
@@ -1,10 +1,47 @@
 <template>
-  <div></div>
+  <v-card :key="media" class="ml-3 mr-3">
+    <v-list-item-subtitle
+        class="pl-3 primary--text" 
+      >
+      <v-row no-gutters align="stretch" class="mt-2">
+        &nbsp;
+        <v-img 
+        max-height="30" 
+        max-width="30" 
+        :src="`/${media}_icon.png`">
+        </v-img>
+        &nbsp;&nbsp;
+        <p class="mt-1">
+          {{likes}}
+          likes
+        </p>
+        <v-spacer></v-spacer>
+        <p class="mt-1">
+          {{date}}
+        </p>
+        &nbsp;
+      </v-row>
+      <p class="text-wrap mt-1">
+      {{comment}}
+      </p>
+      <v-chip class="mb-2 primary trending-category mr-1">
+        {{topic}}
+      </v-chip>
+    </v-list-item-subtitle>
+  </v-card>
 </template>
 
 <script>
 export default {
-  
+  data () {
+    return {
+      media: 'facebook',
+      likes: '65,000',
+      date: '21 December 2021',
+      comment: 'The dreadful breakthrough infection can effectively be prevented with VCO/Lauric Acid, and total community viral load can be lowered, do not play dice with Covid, try to protect the community and your loved ones at the same time.',
+      topic: 'Healthcare',
+    }
+  }
 }
 </script>
 
--- app/vue/components/ComplaintsRelatedComments.vue
+++ app/vue/components/ComplaintsRelatedComments.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
+    <v-row no-gutters align="stretch">
+      &nbsp; &nbsp; 
+      <DropDownSelect 
+      v-bind:viewFilter="sortView" v-bind:label="label" v-bind:viewSelected="viewSelected">
+      </DropDownSelect>
+    </v-row>
+    <ComplaintsRelatedComment/>
+    &nbsp;
     <ComplaintsRelatedComment/>
   </div>
 </template>
 
 <script>
+import DropDownSelect from "./DropDownSelect.vue"
 import ComplaintsRelatedComment from './ComplaintsRelatedComment.vue'
+
 export default {
-  components: { ComplaintsRelatedComment },
-  
+  components: {
+    DropDownSelect, 
+    ComplaintsRelatedComment 
+  },
+  data: () => ({
+      sortView: [{view: 'Likes'}, {view: 'Date'}],
+      label: 'Sort By',
+      // viewSelected: 'Likes',
+  }),
 }
 </script>
 
