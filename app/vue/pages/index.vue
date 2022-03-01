01691f3524c2bb650b4b9054ad2e8cdf9ab819fe
jhin-yee <jhinyee.ong.2019@scis.smu.edu.sg>
2022-03-02 01:42:40 +0800

    update index.vue

--- app/vue/pages/index.vue
+++ app/vue/pages/index.vue
@@ -11,27 +11,62 @@
       class="mx-auto mt-13 mb-n5"
     ></v-img>
 
-    <v-autocomplete
-      v-model="autocompleteModel"
-      :items="items"
-      :loading="isLoading"
-      :search-input.sync="search"
-      clearable
-      hide-details
-      hide-selected
-      item-text="name"
-      item-value="symbol"
-      label="Enter a query!"
-      solo
-      dense
-      return-object
+    <v-card
+      class="mx-auto"
+      max-width="344"
     >
-    </v-autocomplete>
+      <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        height="200px"
+      ></v-img>
+
+      <v-card-title>
+        Top western road trips
+      </v-card-title>
+
+      <v-card-subtitle>
+        1,000 miles of wonder
+      </v-card-subtitle>
+
+      <v-card-actions>
+        <v-btn
+          color="orange lighten-2"
+          text
+        >
+          Explore
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          icon
+          @click="show = !show"
+        >
+          <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+        </v-btn>
+      </v-card-actions>
+
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+
+          <v-card-text>
+            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+    </v-card>
+
   </v-container>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  modules: {
+    filters: {
+      description: 'Filters'
+    }
+  }
 }
 </script>
