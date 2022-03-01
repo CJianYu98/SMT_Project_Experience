4ecd7187f6ab274392900f4696e1e34632353464
jhin-yee <jhinyee.ong.2019@scis.smu.edu.sg>
2022-03-01 11:02:41 +0800

    template for landing page

--- app/vue/pages/index.vue
+++ app/vue/pages/index.vue
@@ -1,8 +1,33 @@
 <template>
-  <v-row justify="center" align="center">
+  <!-- <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
     </v-col>
-  </v-row>
+  </v-row> -->
+  <v-container class="mt-n7">
+    <v-img
+      max-height="300"
+      max-width="300"
+      :src="`/tls_logo_landing_text.png`"
+      class="mx-auto mt-13 mb-n5"
+    ></v-img>
+
+    <v-autocomplete
+      v-model="autocompleteModel"
+      :items="items"
+      :loading="isLoading"
+      :search-input.sync="search"
+      clearable
+      hide-details
+      hide-selected
+      item-text="name"
+      item-value="symbol"
+      label="Enter a query!"
+      solo
+      dense
+      return-object
+    >
+    </v-autocomplete>
+  </v-container>
 </template>
 
 <script>
