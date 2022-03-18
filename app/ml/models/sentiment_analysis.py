bc77ab9cec1abf3080f96d24d303875166549558
joshuawong96 <51185596+joshuawong96@users.noreply.github.com>
2022-03-18 18:04:25 +0800

    Update sentiment_analysis.py

--- app/ml/models/sentiment_analysis.py
+++ app/ml/models/sentiment_analysis.py
@@ -1,8 +1,7 @@
-import numpy as np
+import pandas as pd
 from datasets import Dataset
-from scipy.special import softmax
 from tqdm.auto import tqdm
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
+from transformers import pipeline
 from transformers.pipelines.pt_utils import KeyDataset
 
 
@@ -25,40 +24,18 @@ def classify_emotions(text: str) -> str:
 
 
 ############### SENTIMENT ###############
-def classify_sentiment(df):
+def classify_sentiment(df: pd.DataFrame) -> pd.DataFrame:
+
     dataset = Dataset.from_pandas(df)
     MODEL = "cardiffnlp/twitter-roberta-base-sentiment"
     pipe = pipeline("sentiment-analysis", model=MODEL, tokenizer=MODEL, max_length=512, truncation=True)
 
-    labels_dict = {
-        "LABEL_0": "negative",
-        "LABEL_1": "neutral",
-        "LABEL_2": "positive"
-    }
+    labels_dict = {"LABEL_0": "negative", "LABEL_1": "neutral", "LABEL_2": "positive"}
     labels = []
 
     for out in tqdm(pipe(KeyDataset(dataset, "cleantext"))):
         current_label = out["label"]
         labels.append(labels_dict[current_label])
-    df['sentiment_label'] = labels
-    
+    df["sentiment_label"] = labels
+
     return df
-
-
-
-
-# MODEL = "cardiffnlp/twitter-roberta-base-sentiment"
-# tokenizer = AutoTokenizer.from_pretrained(MODEL)
-# # tokenizer.save_pretrained(MODEL)
-# model = AutoModelForSequenceClassification.from_pretrained(MODEL)
-# # model.save_pretrained(MODEL)
-# labels = ["negative", "neutral", "positive"]
-
-# def classify_sentiment(text: str) -> str:
-#     encoded_input = tokenizer(text, return_tensors="pt")
-#     output = model(**encoded_input)
-#     scores = output[0][0].detach().numpy()
-#     scores = softmax(scores)
-#     ranking = np.argsort(scores)
-#     ranking = ranking[::-1]
-#     return labels[ranking[0]]
