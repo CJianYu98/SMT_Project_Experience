aac540699c4f7bca048ff4b13bcb20a8c1ba9815
joshuawong96 <51185596+joshuawong96@users.noreply.github.com>
2022-03-22 10:25:43 +0800

    update ml scripts

--- app/ml/models/intent_classification.py
+++ app/ml/models/intent_classification.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from transformers import pipeline
 
-# Zero-shot Intent Classification Model 
-classifier = pipeline("zero-shot-classification")
+# Zero-shot Intent Classification Model
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
 
 def classify_intent(text):
     labels = ["suggestion", "complaint", "educational", "question", "remark"]
 
-    results_dict = classifier(text, 
-                    labels)
+    results_dict = classifier(text, labels)
 
     labels = results_dict["labels"]
     intent = labels[0]
