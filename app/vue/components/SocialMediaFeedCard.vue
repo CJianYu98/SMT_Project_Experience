c6f2ab57dfce121685c56a99d9ff057e1fdb08d0
jhin-yee <jhinyee.ong.2019@scis.smu.edu.sg>
2022-03-25 13:17:40 +0800

    create social media feed page

---
+++ app/vue/components/SocialMediaFeedCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card
+    elevation="3"
+    class="mx-8 mr-2 mt-8 rounded-lg"
+    height="100%"
+  >
+    <v-card-title class="pb-4 accent--text text-h6 mx-4">
+      <v-img
+        max-height="30"
+        max-width="30"
+        src="/facebook_icon.png"
+        alt="facebook icon"
+        class="mr-3"
+      ></v-img>
+        <!-- class="mx-auto" -->
+        <!-- :src="`/${platform}_icon.png`"
+        :alt="`${platform} icon`" -->
+      Facebook
+      <!-- {{ platform.name }} -->
+    </v-card-title>
+    <RelatedComments :related-comments="relatedComments"/>
+  </v-card>
+</template>
+
+<script>
+import RelatedComments from './RelatedPosts.vue'
+export default {
+  components: { 
+    RelatedComments ,
+  },
+  props: {
+    platform: {
+      type: Object,
+      required: true,
+    },
+    relatedComments: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+
+<style scoped>
+
+</style>
+
