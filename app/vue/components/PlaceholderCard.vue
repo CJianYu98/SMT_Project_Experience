e32c66f360cb7c0483fd8d9fdd51959844c062e4
jhin-yee <jhinyee.ong.2019@scis.smu.edu.sg>
2022-04-07 20:41:20 +0800

    create placeholder card

---
+++ app/vue/components/PlaceholderCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card
+    elevation="3"
+    class="ml-8 mr-2 mt-8 rounded-lg  d-flex align-center"
+    height="100%"
+  >
+    <v-img
+      max-height="450"
+      max-width="450"
+      :src="`/placeholderImage.png`"
+      :alt="`placeholderImage`"
+      class="mx-auto"
+    ></v-img>
+  </v-card>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+
+<style scoped>
+
+</style>
+
+
+
